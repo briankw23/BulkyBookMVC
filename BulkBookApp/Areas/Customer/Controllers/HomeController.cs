c8473152bc7e729722e7ff1cb793015c3400156b
Brian Wright <briankw23@gmail.com>
2021-01-23 09:43:13 -0600

    Section 5, setup complete

--- BulkBookApp/Areas/Customer/Controllers/HomeController.cs
+++ BulkBookApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkBookApp.Models;
+using BulkBookApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,8 +8,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BulkBookApp.Controllers
+namespace BulkBookApp.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
